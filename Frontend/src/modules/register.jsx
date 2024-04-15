5c7da9dc326e63f27d7efaa2d07fce6e095b1189
cchangala@umass.edu <cchangala@umass.edu>
2024-04-15 04:27:45 -0400

    Initial commit

--- Frontend/src/modules/register.jsx
+++ Frontend/src/modules/register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Web3 from 'web3'
 import '../css/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'slick-carousel/slick/slick.css'; 
@@ -7,18 +8,144 @@ import { FiEye } from "react-icons/fi";
 import { FiEyeOff } from "react-icons/fi";
 
 const RegisterForm = () => {
+
+    const [formData, setFormData] = useState({
+        FirstName: '',
+        LastName: '',
+        PhoneNumber:'',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
     const [showPassword, setShowPassword] = useState(false);
-    const [FirstName, setFirstName] = useState('');
-    const [LastName, setLastName] = useState('');
-    const [PhoneNumber, setPhoneNumber] = useState('');
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
 
-    const submitRegister = () => {
-        console.log("Submitted");
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
     };
 
+    const projectId = '6bc42b50d520477f99374ca081d7a0c5';
+    const infuraUrl = `https://polygon-amoy.infura.io/v3/6bc42b50d520477f99374ca081d7a0c5`;
+
+    const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl, {
+        headers: [{ name: "Authorization", value: "Basic "}]
+      }));
+
+    const address = "0xb02992b422bB7377B0b3ee8dd04Fc3ea6e900823";
+    const abi =  [
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_email",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_password",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_confirmPassword",
+                    "type": "string"
+                }
+            ],
+            "name": "createAccount",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_email",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_password",
+                    "type": "string"
+                }
+            ],
+            "name": "loginAccount",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "users",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "email",
+                    "type": "string"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "hashedPassword",
+                    "type": "bytes32"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+    const contract = new web3.eth.Contract(abi,address);
+
+    const account = web3.eth.accounts.privateKeyToAccount('0x51a18a3afa87e9833a04d67dc201244f5e92fd17fd98d4221532281d5b21438c');
+    web3.eth.accounts.wallet.add(account);
+    web3.eth.defaultAccount = account.address;
+
+    const submitRegister = (event) => {
+        event.preventDefault(); // Prevent the default form submission
+        console.log(account.address);
+        if (formData.password !== formData.confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+        try {
+            // Example of a contract function call
+            contract.methods.createAccount(formData.email, formData.password,formData.confirmPassword)
+            .send({from: account.address, gas: 3000000}).then(result => {
+                console.log('Transaction successful: ', result);
+                alert('Registration successful!');
+              })
+              .catch(error => {
+                console.error('Transaction failed: ', error);
+                alert('Registration failed! Already existing user or wrong details entered');
+                if (error.receipt) {
+                  console.log('Transaction receipt: ', error.receipt); // Provides more details about the transaction failure
+                }
+              });
+              
+            console.log("Registration Submitted", formData);
+        } catch (error) {
+            console.error(error);
+            alert('Registration failed. See console for details.');
+        }
+    };
     return (
         <>
         <section>
@@ -32,9 +159,9 @@ const RegisterForm = () => {
                             id="FirstName"
                             name="FirstName"
                             placeholder="Enter your First Name"
-                            value={FirstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                            required=""          
+                            value={formData.FirstName}
+                            required={true}
+                            onChange={handleChange}        
                         />
                     </div>
                     <div className="form-group">
@@ -44,9 +171,9 @@ const RegisterForm = () => {
                             id="LastName"
                             name="LastName"
                             placeholder="Enter your Last Name"
-                            value={LastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            required=""          
+                            value={formData.LastName}
+                            onChange={handleChange}
+                            required=""         
                         />
                     </div>
                     <div className="form-group">
@@ -56,8 +183,8 @@ const RegisterForm = () => {
                             id="PhoneNumber"
                             name="PhoneNumber"
                             placeholder="Enter your Phone Number"
-                            value={PhoneNumber}
-                            onChange={(e) => setPhoneNumber(e.target.value)}
+                            value={formData.PhoneNumber}
+                            onChange={handleChange} 
                             required=""          
                         />
                     </div>
@@ -69,6 +196,8 @@ const RegisterForm = () => {
                             name="email"
                             placeholder="Enter your email"
                             required=""
+                            onChange={handleChange}
+                            value={formData.email}
                         />
                     </div>
                     <div className="form-group password-group">
@@ -79,6 +208,8 @@ const RegisterForm = () => {
                             name="password"
                             placeholder="Enter your password"
                             required=""
+                            onChange={handleChange}
+                            value={formData.password}
                         />
                     </div>
                     <div className="form-group password-group">
@@ -89,6 +220,8 @@ const RegisterForm = () => {
                             name="confirmPassword"
                             placeholder="Confirm your password"
                             required=""
+                            onChange={handleChange}
+                            value={formData.confirmPassword}
                         />
                         <div onClick={togglePasswordVisibility} style={{ cursor: 'pointer', display: 'inline-block', paddingTop: "10px" }}>
                             {showPassword ? 
